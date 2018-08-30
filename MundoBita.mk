66e8d2737f75402ae272596d42b193af0fb6d47e
Luiz Normando Zandonai Ferreira <luiznormando@graphlab.com.br>
2018-08-30 16:20:20 -0300

    Added 'MundoBita' channel.

---
+++ MundoBita.mk
@@ -0,0 +1,127 @@
+#
+$(info MundoBita)
+#
+# 2018 (C) GraphLab Computação Ltda.
+#
+# Mr.Plot Videos
+# https://www.youtube.com/user/mrplotvideos
+#
+
+
+#
+# Files:
+#
+
+#
+# 12 vídeos
+# "Bita e a Natureza"
+# playlist: PLt7PgJ6tMUQR4deNAQxMrxvyYcPgPGOtS
+#
+NATUREZA =          EfVmLO0y0AU EumDNDsW4RA cM1Q0Riguew haI0KhtP2yM \
+                    YHbEAvtmmbs TdeZu4N1FY0 mfFPh3RFovk s6hFi6aUBoQ \
+                    ntReWqrlYbE rUeaT5eqCyg 8nZLhuef4QM k7rcvY17W6c
+
+#
+# 11 vídeos
+# "Bita e as Brincadeiras"
+# playlist: PLt7PgJ6tMUQQSOb-Hu8AYq7A75hVESVNp
+#
+BRINCADEIRAS =      4hmTy4mTCtc tyHj0OxckSc QvP6h2vLPyI AD25rGt5x00 \
+                    JTJfJ0jWQGo 2l9nXqvhy6U OCyKKSjCzKY xPQpagdYKNs \
+                    _HsZ2vPksvE Ey2eFH8NNQk zL5nyXPjJjU
+
+#
+# 6 vídeos
+# "Bita e o Corpo Humano"
+# playlist: PLt7PgJ6tMUQQ2YjIi79-ZZK_d_a0Aya52
+#
+CORPO_HUMANO =      eLtzvypcurE AFN52hq7d7w _kjh0Pwc2Ms dqSanOdW4Iw \
+                    FhNJHE0aask 7RTUn5n6yvA
+
+#
+# 10 vídeos
+# "Bita e o Nosso Dia"
+# playlist: PLt7PgJ6tMUQQBusJquECMicdCfCGVZaPx
+#
+NOSSO_DIA =         FY2-Zghwgoo -RsLzUsXYHM swFgd3rs7Gc 31iBxkTTAfc \
+                    GivehL1pBag oChO-C4__ZE BFCePyavZbg cnzgHAIRqmc \
+                    mp57Li8mFpI nz0F4nP4ekw
+
+#
+# 7 vídeos
+# "Bita e o Nosso Mundo"
+# playlist: PLt7PgJ6tMUQQCSRgI-_gb2nCGcDPJioHR
+#
+NOSSO_MUNDO =       Dr4gittIyaU YTAnjjaeVzU FH4EDYF1frI N837uUhxudo \
+                    M71yKfSkyJA 6kAuKDpIaww 1TuiHeNltbo K9R5OCocIp4
+
+#
+# 11 vídeos
+# "Bita e os Animais"
+# playlist: PLt7PgJ6tMUQSk6C1ZyFackbi6T1xTy7a2
+#
+ANIMAIS =           2ZB6dQ-Fjho 5TVsXxsFJps 0GIgk4yuHOQ cjONzZPJONc \
+                    Oa5BaUBXpho j7A9ANT2aVQ B9gps8zg4U0 iY91JoMWQoM \
+                    DysjscnMU5g 9WFYuIu7BKA hgFfC4cCnec
+
+#
+# 7 vídeos
+# "Especiais Mundo Bita"
+# playlist: PLt7PgJ6tMUQTrZ8VyCYTfwj6qqqrN6qeM
+#
+ESPECIAIS =         Vw2nI5tFjQs Tvq1tHHSi5I siVfbM-jicg eulFsmcNHos \
+                    cGs1QA2bLdw _9EtOUoELtk ueTMLzcYcu0
+
+#
+# 11 vídeos
+# "Férias com Mundo Bita"
+# playlist: PLt7PgJ6tMUQQLqec2rqFZU0fxVstuRpgr
+#
+FERIAS =            AFN52hq7d7w uUgv2bCnbv4 tyHj0OxckSc 5TVsXxsFJps \
+                    0GIgk4yuHOQ AD25rGt5x00 dqSanOdW4Iw FhNJHE0aask \
+                    DysjscnMU5g zL5nyXPjJjU cnzgHAIRqmc
+
+#
+# 18 vídeos
+# "Promocionais & Apps"
+# playlist: PLt7PgJ6tMUQQqd9Oa2GUP98YSL807Aipt
+#
+PROMOCIONAIS =      zUsQbjg9QuM IoeuWEXaRMM Ci2y5UVXh_g WY07hyc_sqk \
+                    Syt77jrpb08 YE3jGUyEQFc DrxgnYwXAKA xqgqwDc64WQ
+
+#
+# 30 vídeos
+# "MundoBitaVEVO"
+# playlist: PLt7PgJ6tMUQR_UkKd-D5O6XAcHluXTHZH
+#
+VEVO =              4hmTy4mTCtc eLtzvypcurE AFN52hq7d7w swFgd3rs7Gc \
+                    Vw2nI5tFjQs 31iBxkTTAfc siVfbM-jicg tyHj0OxckSc \
+                    5TVsXxsFJps 0GIgk4yuHOQ _kjh0Pwc2Ms QvP6h2vLPyI \
+                    AD25rGt5x00 Oa5BaUBXpho j7A9ANT2aVQ JTJfJ0jWQGo \
+                    dqSanOdW4Iw 2l9nXqvhy6U zgynwblKtJQ B9gps8zg4U0 \
+                    OCyKKSjCzKY oChO-C4__ZE DysjscnMU5g xPQpagdYKNs \
+                    _HsZ2vPksvE BFCePyavZbg zL5nyXPjJjU cnzgHAIRqmc
+
+#
+# Rules:
+#
+
+MUNDO_BITA        = MundoBita
+MUNDO_BITA_DIR    = $(BASE_DIR)/$(MUNDO_BITA)
+MUNDO_BITA_VIDEOS = $(NATUREZA) $(BRINCADEIRAS) $(CORPO_HUMANO) \
+                    $(NOSSO_DIA) $(NOSSO_MUNDO) $(ANIMAIS) $(ESPECIAIS) \
+                    $(FERIAS) $(VEVO) $(PROMOCIONAIS)
+
+
+$(MUNDO_BITA)_all: $(MUNDO_BITA)_dir $(patsubst %, $(MUNDO_BITA_DIR)/.%.fmt, $(MUNDO_BITA_VIDEOS))
+
+$(MUNDO_BITA)_dir:
+	@mkdir --parents --verbose $(MUNDO_BITA_DIR)/
+
+$(MUNDO_BITA)_clean:
+	@rm --recursive --force --verbose $(MUNDO_BITA_DIR)/
+
+
+TARGET_ALL   += $(MUNDO_BITA)_all
+TARGET_CLEAN += $(MUNDO_BITA)_clean
+DOT_PHONY    += $(MUNDO_BITA)_all $(MUNDO_BITA)_dir $(MUNDO_BITA)_clean
