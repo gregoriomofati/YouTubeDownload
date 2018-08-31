b9c8ed0f9fb57bcf81faf18afb40f1ff840d9f5a
Luiz Normando Zandonai Ferreira <luiznormando@graphlab.com.br>
2018-08-31 18:18:36 -0300

    Added 'LooLooKids' channe.

---
+++ LooLooKids.mk
@@ -0,0 +1,154 @@
+#
+$(info LooLooKids)
+#
+# 2018 (C) GraphLab Computação Ltda.
+#
+
+
+#
+# Files:
+#
+
+#
+# 40 videos
+# Rainbow Chicks - Cartoons for Children (All episodes)
+# playlist: PLFkIjhjdbaAc987Ib4onfcpztAQnaSfCw
+# errors: 
+#
+RAINBOW_CHICKS    = LwR-u2YO8NI cRrmakZFuI0 W72AgKIjdXI eE50qWWS1yM \
+                    X9pdoWPOcVY xXdrSSFZqiM DUlv05ALS34 GQPQlFPqnqo \
+                    CnNUteww2Vo GdObtUcL7NQ qdkLPHgO_oY b71_hbHGD7g \
+                    h9yTprxQwd4 yv6pgeh2kxA xb6jabdpmfI EJENvS60_Rc \
+                    W6v7DCqfiNc PC4q-DXBCrU _YbruYMQ1Es xttbZ5Fhkmc \
+                    X407mX5JckE p7BueftbEsI jNer1A43_ds PEnrUbAhA3w \
+                    ijbd8y1tRrQ xh-vSYcEeyc PUEy1ox48DE vvHRkB3OQoc \
+                    OICkVlmKaOM ytFVvwG88TU ULQKLoPzHpI uUQ_lscdXqY \
+                    5TLl8K91FqQ ZIFR2N-ktjw 3G7itkaaVzY TsM5ZzOeEMY \
+                    g8KhWigUUlc 9JK2--sG6fA ueQdrpY86c8 WG-z5Ipim5E \
+                    YgABJ_rHoho
+
+#
+# 80 videos
+# Educational Songs
+# playlist: PLFkIjhjdbaAdon48GLroq2Id8IFYbV-zJ
+# errors: Zz6PbNj10iU
+#
+EDUCATIONAL_SONGS = 7mPZXMkqFL8 yNIY9_u9MJM jk7N3bKvgvg ovfdRHdGR_w \
+                    cqhD-j7pzYM WoKJSN-H4NU Bvo-73ILr_A ARiHneKb_U0 \
+                    _rac7Eo--_A 9aAktIROuaw 6X3A-Qe4lPg u8R8_49SydE \
+                    nCqUYAnQF0o zMG2AimfSaE -ccCPcujnys nrqcZZ1Nbw4 \
+                    zEFaBBzMd1U v5MuuuMFKK4 lJpKEOdlRQg UZSRYKWHwkw \
+                    kpd0mUEjQ44 F4tHL8reNCs 9c5fW_4o7Bo cKv5z9eHcWk \
+                    oijharQMbSQ 15pfO2zq-xY MY5C8FIVfgs dDgJsd-9wJA \
+                    NLsKQ48b9zQ HzKCthegHdU CzFUhI7GQM0 uw23TVH4O3M \
+                    ptXUH9vhCmA HNTHgQiZNm4 fazGNf6Cbuw uT1JkIbAwYE \
+                    9s0dAxDf4XY 5v3DsAfEFrU ugTcjCNA2x0 u23QeiPMJFA \
+                    uYKPdXRqsdg vs-6A_mS4vk _TgOLwu9WRc 44jnX8-PhHs \
+                    5p8vfBTxBFM u2D-x_DH9co 6YqEUdI7Wic XU2AhM5rSmM \
+                    GOMO41Oa98k 2Lpo-BWmJaU r_od4LfYym8 _RFkCULiXi8 \
+                    gyegm85BPPA Mcwj3kXpo34 gvspwu2-I3A lfErfnW91iE \
+                    SgmPyRQnmgE 8DQxBdHyGVg QjBXrgUwy5o 6iqbdqIR4XU \
+                    6MuZbyAclaE -fyS-DvkoPQ bM8d-jXkhao bKPSfOlKHbM \
+                    _3NMv6rfxcY 2w-ZTq0ZDkA YgABJ_rHoho 8b0DWKYaGvQ \
+                    AkRNe7byrOA 4QbxSd1tL8Q o3xIzNwcpm4 ScJxjKKATfA \
+                    5YccYb5jOVc tDiQ59LwQyk Jp6sp_7r4p4 dI-SDmaScvc \
+                    SeeyLi33Yg4 qYY-BbEZJQc 9pH8QPikre8 EsKNt0dLGJU 
+
+#
+# 7 videos
+# Lullabies
+# playlist: PLFkIjhjdbaAcIITDaDLS6e6msG3v8GFze
+# errors: 
+#
+LULLABIES         = FFWkc4Adno4 -saC-CpszRs dJyIyWq8Fpw x96KZ2Gd2JY \
+                    KWALvJtKI-k abJ3n6EYHxg rH5UKvebwpw
+
+#
+# 85 videos
+# New Songs
+# playlist: PLFkIjhjdbaAd96H-bsXZh_aeVKQbVlBiR
+# errors: Zz6PbNj10iU
+#
+NEW_SONGS         = qYY-BbEZJQc SeeyLi33Yg4 dI-SDmaScvc Jp6sp_7r4p4 \
+                    tDiQ59LwQyk 5YccYb5jOVc ScJxjKKATfA o3xIzNwcpm4 \
+                    4QbxSd1tL8Q AkRNe7byrOA 8b0DWKYaGvQ YgABJ_rHoho \
+                    2w-ZTq0ZDkA _3NMv6rfxcY bM8d-jXkhao -fyS-DvkoPQ \
+                    6iqbdqIR4XU 8DQxBdHyGVg lfErfnW91iE gvspwu2-I3A \
+                    uw23TVH4O3M dJyIyWq8Fpw F4tHL8reNCs 2Lpo-BWmJaU \
+                    r_od4LfYym8 cqhD-j7pzYM XU2AhM5rSmM zEFaBBzMd1U \
+                    uYKPdXRqsdg 5p8vfBTxBFM CDtihdIc8rA Bvo-73ILr_A \
+                    NqfnGQIOr_Y GOMO41Oa98k _RFkCULiXi8 FFWkc4Adno4 \
+                    vs-6A_mS4vk nrqcZZ1Nbw4 u2D-x_DH9co HzKCthegHdU \
+                    ptXUH9vhCmA u23QeiPMJFA lJpKEOdlRQg 6YqEUdI7Wic \
+                    44jnX8-PhHs jk7N3bKvgvg 6X3A-Qe4lPg oijharQMbSQ \
+                    gyegm85BPPA ARiHneKb_U0 MY5C8FIVfgs kpd0mUEjQ44 \
+                    ugTcjCNA2x0 7mPZXMkqFL8 nCqUYAnQF0o 9c5fW_4o7Bo \
+                    EPOi5Sfb57E 15pfO2zq-xY abJ3n6EYHxg 9aAktIROuaw \
+                    cKv5z9eHcWk CzFUhI7GQM0 KWALvJtKI-k -saC-CpszRs \
+                    uT1JkIbAwYE v5MuuuMFKK4 u8R8_49SydE UZSRYKWHwkw \
+                    ovfdRHdGR_w _rac7Eo--_A yNIY9_u9MJM fazGNf6Cbuw \
+                    _TgOLwu9WRc -ccCPcujnys WoKJSN-H4NU 5v3DsAfEFrU \
+                    HNTHgQiZNm4 x96KZ2Gd2JY rH5UKvebwpw zMG2AimfSaE \
+                    NLsKQ48b9zQ 9s0dAxDf4XY dDgJsd-9wJA 9pH8QPikre8 \
+                    EsKNt0dLGJU 
+
+#
+# 25 videos
+# Top 25 Songs
+# playlist: PLFkIjhjdbaAfxlRXC3-WmIbNZY0mL7duL
+# errors: 
+#
+TOP_25_SONGS      = F4tHL8reNCs 6X3A-Qe4lPg nCqUYAnQF0o jk7N3bKvgvg \
+                    ptXUH9vhCmA -ccCPcujnys UZSRYKWHwkw -saC-CpszRs \
+                    WoKJSN-H4NU 9c5fW_4o7Bo ovfdRHdGR_w zMG2AimfSaE \
+                    _rac7Eo--_A MY5C8FIVfgs lJpKEOdlRQg 9aAktIROuaw \
+                    x96KZ2Gd2JY HzKCthegHdU 15pfO2zq-xY cKv5z9eHcWk \
+                    ARiHneKb_U0 gyegm85BPPA u8R8_49SydE fazGNf6Cbuw \
+                    KWALvJtKI-k
+
+#
+# 32 videos
+# Animal Songs
+# playlist: PLFkIjhjdbaAf2pErcYmc0XOVi-447mkuE
+# errors: 
+#
+ANIMAL_SONGS      = -ccCPcujnys 6YqEUdI7Wic lJpKEOdlRQg 2Lpo-BWmJaU \
+                    r_od4LfYym8 zMG2AimfSaE uYKPdXRqsdg 5p8vfBTxBFM \
+                    u23QeiPMJFA GOMO41Oa98k _RFkCULiXi8 vs-6A_mS4vk \
+                    u2D-x_DH9co 44jnX8-PhHs oijharQMbSQ gyegm85BPPA \
+                    MY5C8FIVfgs WoKJSN-H4NU fazGNf6Cbuw 7mPZXMkqFL8 \
+                    _TgOLwu9WRc 9aAktIROuaw _rac7Eo--_A dDgJsd-9wJA \
+                    lfErfnW91iE 8DQxBdHyGVg bM8d-jXkhao bKPSfOlKHbM \
+                    4QbxSd1tL8Q Jp6sp_7r4p4 dI-SDmaScvc qYY-BbEZJQc 
+
+#
+# 419 videos
+# The Best Nursery Rhymes in English
+# playlist: PLFkIjhjdbaAdmPyradNYGNG5vbbPQBuuw
+# errors: 
+#
+THE_BEST_NURSERY_RHYMES_IN_ENGLISH = 
+
+#
+# Rules:
+#
+
+LOOLOO_KIDS       = LooLooKids
+LOOLOO_KIDS_DIR   = $(BASE_DIR)/$(LOOLOO_KIDS)
+LOOLOO_KIDS_FILES = $(RAINBOW_CHICKS) $(EDUCATIONAL_SONGS) $(LULLABIES) \
+                    $(NEW_SONGS) $(TOP_25_SONGS) $(ANIMAL_SONGS) \
+                    $(THE_BEST_NURSERY_RHYMES_IN_ENGLISH)
+
+
+$(LOOLOO_KIDS)_all: $(LOOLOO_KIDS)_dir $(patsubst %, $(LOOLOO_KIDS_DIR)/.%.fmt, $(LOOLOO_KIDS_FILES))
+
+$(LOOLOO_KIDS)_dir:
+	@mkdir --parents --verbose $(LOOLOO_KIDS_DIR)/
+
+$(LOOLOO_KIDS)_clean:
+	@rm --recursive --force --verbose $(LOOLOO_KIDS_DIR)/
+
+
+TARGET_ALL   += $(LOOLOO_KIDS)_all
+TARGET_CLEAN += $(LOOLOO_KIDS)_clean
+DOT_PHONY    += $(LOOLOO_KIDS)_all $(LOOLOO_KIDS)_dir $(LOOLOO_KIDS)_clean
