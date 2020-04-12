534e35494ccfa695f09d0264a1c7bb42c8fb3d8d
Gregório Mofati C. Z. Ferreira <gregorio@graphlab.com.br>
2020-04-12 10:45:00 -0300

    Added 'Nosso Mundo - Aquilo que te Move' released on Apr 3rd, 2020.

--- MundoBita.mk
+++ MundoBita.mk
@@ -57,11 +57,11 @@ NOSSO_MUNDO =       Dr4gittIyaU YTAnjjaeVzU FH4EDYF1frI N837uUhxudo \
                     QoB7gWNchtc EW1Is3BVp5U OEUXZ2uz1a4 s48phnrkZ5w
 
 #
-# 3 vídeos
+# 4 vídeos
 # "Bita e o Nosso Mundo 2"
 # playlist: N.A.
 #
-NOSSO_MUNDO +=      8Z3FDAt9zDw jmQoDtE3o1Y eiPE7YO_F30
+NOSSO_MUNDO +=      8Z3FDAt9zDw jmQoDtE3o1Y eiPE7YO_F30 AlH-BTDX22Q
 
 #
 # 11 vídeos
